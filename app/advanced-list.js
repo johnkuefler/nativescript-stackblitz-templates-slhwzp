91c435bfc085172ba233780a895417a5633f23b6
johnkuefler <john@dev-squared.com>
2023-11-03 12:15:22 -0500

    added axios

---
+++ app/advanced-list.js
@@ -0,0 +1,24 @@
+const ObservableArray = require("@nativescript/core/data/observable-array").ObservableArray;
+const frameModule = require("@nativescript/core/ui/frame");
+const axios = require('axios');
+
+exports.pageLoaded = async function(args) {
+  const page = args.object;
+  const listView = page.getViewById("myListView");
+
+  const userData = await axios.get('https://jsonplaceholder.typicode.com/users')
+
+  listView.items = userData;
+};
+
+exports.onItemTap = function(args) {
+  const listView = args.object;
+  const index = args.index;
+  const tappedItem = listView.items.getItem(index);
+
+  frameModule.Frame.topmost().navigate({
+    moduleName: "detail-page",
+    context: tappedItem,
+    animated: true
+  });
+};
