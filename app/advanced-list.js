926963ebe20a25b310d0b9def6c53ff24c979768
johnkuefler <john@dev-squared.com>
2023-11-03 12:39:03 -0500

    added colors

--- app/advanced-list.js
+++ app/advanced-list.js
@@ -5,12 +5,31 @@ exports.pageLoaded = async function(args) {
   const page = args.object;
   const listView = page.getViewById("myListView");
 
-  //const userData = await axios.get('https://jsonplaceholder.typicode.com/users')
 
-  const userData = [];
+  let userData = await getUserData();
+
+  console.log(userData);
 
   listView.items = userData;
 };
+
+async function getUserData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  let data = await response.json();
+  
+  data.forEach(user => {
+    let [firstName, lastName] = user.name.split(' ');
+    user.initials = `${firstName[0]}${lastName[0]}`;
+  });
+
+  let colors = ['background-color:blue', 'background-color:orange','background-color:green','background-color:red','background-color:yellow']
+
+  data.forEach(user => {
+    user.squareColor = colors[Math.floor(Math.random() * colors.length)];
+  });
+
+  return data;
+}
 
 exports.onItemTap = function(args) {
   const listView = args.object;
